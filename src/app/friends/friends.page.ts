78195af9a5b24349baee6d9f8f5677945f059787
JannGL <32489666+JannGL@users.noreply.github.com>
2018-12-03 03:36:49 -0600

    lk

---
+++ src/app/friends/friends.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FriendsService } from '../friends.service';
+
+@Component({
+  selector: 'app-friends',
+  templateUrl: './friends.page.html',
+  styleUrls: ['./friends.page.scss'],
+})
+export class FriendsPage implements OnInit {
+
+  constructor(private friendsService: FriendsService) { }
+
+  ngOnInit() {
+    this.friendsService.getData('top-headlines?sources=buzzfeed').subscribe(data => {console.log(data);
+    })
+  }
+
+}
