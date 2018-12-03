e50c907baeaa176daff5720f5c4ecad626654c2e
JannGL <32489666+JannGL@users.noreply.github.com>
2018-12-03 05:31:12 -0600

    iamganes porfin

--- src/app/friends/friends.page.ts
+++ src/app/friends/friends.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FriendsService } from '../friends.service';
 
+
+
+
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.page.html',
@@ -8,10 +11,14 @@ import { FriendsService } from '../friends.service';
 })
 export class FriendsPage implements OnInit {
 
-  constructor(private friendsService: FriendsService) { }
+  constructor(
+    
+    private friendsService: FriendsService) { }
 
   ngOnInit() {
+   
     this.friendsService.getData('top-headlines?sources=buzzfeed').subscribe(data => {console.log(data);
+     
     })
   }
 
